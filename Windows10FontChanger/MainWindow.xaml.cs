a79116b54edf277579a8967665d5d9cd9aeb1333
nnra6864 <enenragd@gmail.com>
2022-12-22 17:08:13 +0100

    Removed unnecessary fonts from the dictionary

--- Windows10FontChanger/MainWindow.xaml.cs
+++ Windows10FontChanger/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace Windows10FontChanger
 
         Dictionary<string, string> _fontsToChange = new Dictionary<string, string>
         {
-            { "Segoe UI(TrueType)", "segoeui.ttf" },
             { "Segoe UI (TrueType)", "segoeui.ttf" },
             { "Segoe UI Black (TrueType)", "seguibl.ttf" },
             { "Segoe UI Black Italic (TrueType)", "seguibli.ttf"},
